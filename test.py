748fe16a58891f877233e8ac10846745360b4b6f
Frank Yeats <louielouiezpc@gmail.com>
2020-02-16 14:32:54 +0800

    enter

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
 import requests
-def getHTMLText(url):
-    try:
-        r=requests.get(url,timeout=20)
-        r.raise_for_status()   # 如果状态不是200，引发HTTPError异常
-        r.encoding=r.apparent_encoding
-        return r.text
-    except:
-        return '产生异常'
 
-if __name__ == "__main__":
-    url='www.baidu.com'
-    print(getHTMLText(url))
+r=requests.post('http://httpbin.org/post',data='ABC')   # 向URL POST一个字符串，自动编码为data（数据）
+print(r.text)
+
+
+# Output:
+
