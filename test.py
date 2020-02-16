3f144f233caed120786736048426879c3831b835
Frank Yeats <louielouiezpc@gmail.com>
2020-02-16 15:05:41 +0800

    enter

--- test.py
+++ test.py
@@ -3,9 +3,11 @@ import requests
 
 kv={'key1':'value1','key2':'value2'}
 
-r=requests.request('http://python123.io/ws',params=kv)   
-print(r.text)
+r=requests.request('GET','http://python123.io/ws',params=kv)   
+print(r.url)
 
 
 # Output:
+https://python123.io/ws?key1=value1&key2=value2
+
 
