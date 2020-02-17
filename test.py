603a481287449e6198ff3d9f9f4f0b7a1a86c639
Frank Yeats <louielouiezpc@gmail.com>
2020-02-17 19:18:52 +0800

    enter

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
 import requests
-from requests.exceptions import ReadTimeout,ConnectTimeout,RequestException
 
 
-try:
-    response=requests.get('http://httpbin.org/get',timeout=0.5)
-    print(response.status_code)
-except ReadTimeout:
-    print('Timeout')    # 超时异常
-except ConnectTimeout:
-    print('Connection error')    # 连接异常
-except RequestException:
-    print('Error')      # 请求异常
+url='http://httpbin.org/get'
+
+response=requests.get('http://httpbin.org/get')
+
