ad7705eeac04b8c563b6a76a3b0d87e2bfa9fc67
Frank Yeats <louielouiezpc@gmail.com>
2020-02-17 19:38:35 +0800

    enter

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
+
+```python
 import requests
 
 
 url='http://httpbin.org/get'
 
-response=requests.get('http://httpbin.org/get')
+r=requests.get(url)
+print(r.status_code)
+x=r.headers.items()
+for k,v in x:
+    print('{}:{}'.format(k,v))
 
+
+# Output:
+200
+Date:Mon, 17 Feb 2020 11:29:51 GMT
+Content-Type:application/json
+Content-Length:306
+Connection:keep-alive
+Server:gunicorn/19.9.0
+Access-Control-Allow-Origin:*
+
+```
