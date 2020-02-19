7bc0e641b7952be01e447720a5c9f79e68f18bf6
Frank Yeats <louielouiezpc@gmail.com>
2020-02-19 21:09:14 +0800

    enter

---
+++ 3.1爬取实操/1.页面爬取.py
@@ -0,0 +1,12 @@
+
+# 
+import requests
+url='https://item.jd.com/100003859236.html#none'
+
+try:
+    r=requests.get(url)
+    r.raise_for_status()
+    print(r.status_code)
+    print(r.text)
+except:
+    print('something error!!!')
