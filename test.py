fcf3c68bb25109f8e946d1ad86b04d380b346d9d
Frank Yeats <louielouiezpc@gmail.com>
2020-02-19 17:37:31 +0800

    enter

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
 import requests
-from requests.exceptions import ReadTimeout
-
-try:
-    r=requests.get('HTTP://www.baidu.com',timeout=0.01)
-    print(r.headers)
-except ReadTimeout:
-    print('request is timeout!!!')
+r=requests.get('https://www.12306.cn',verify=False)
+print(r.status_code)
