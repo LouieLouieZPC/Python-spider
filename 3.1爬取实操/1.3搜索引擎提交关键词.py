7c006fc40b6a0ad53998f31e6901bbdf459a365a
Frank Yeats <louielouiezpc@gmail.com>
2020-02-20 22:01:25 +0800

    enter

--- 3.1爬取实操/1.3搜索引擎提交关键词.py
+++ 3.1爬取实操/1.3搜索引擎提交关键词.py
@@ -3,17 +3,18 @@
 import requests
 from fake_useragent import UserAgent
 ua=UserAgent()
-url='http://www.baidu.com/s'
+url='http://www.so.com/s'
 kv={'user-agent':ua.random}
-ask={'wd':'python'}
+ask={'q':'python'}
 
 try:
     r=requests.get(url,params=ask,headers=kv)
     r.raise_for_status()
     r.encoding=r.apparent_encoding
     print(r.status_code)
-    print(r.request.url)
+    print(r.request.url)             # 查看请求request的url
     print(r.request.headers)
+    print(r.text)
 
 except:
     print('Something Error!!!')
