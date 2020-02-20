8d6fedf9b2c2e5f9fb16e05ab4f0457ef65d4f43
Frank Yeats <louielouiezpc@gmail.com>
2020-02-20 22:21:22 +0800

    enter

--- 3.1爬取实操/1.4网络图片的爬取与存储.py
+++ 3.1爬取实操/1.4网络图片的爬取与存储.py
@@ -1,6 +1,12 @@
 import requests
-url='http://www.baidu.com/s'
-kv={'wd':'你好'}
-r=requests.get(url,params=kv)
-print(r.status_code)
-print(r.request.url)
+import os
+
+path='D:\01.Software\GitHub\GitHub Repository\Python-spider\3.2案例'
+url='http://www.runoob.com/wp-content/uploads/2019/02/xcc8bae8c5bb5b3f01a9f8b37a24dd25295e7b66d.jpg'
+
+r=requests.get(url)
+
+
+with open(path,'wb') as f:
+    f.write(r.content)
+
