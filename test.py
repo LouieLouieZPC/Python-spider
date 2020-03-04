d89fa0fe7874b79b5d22ac25ea3c2a73f530deb9
Frank Yeats <louielouiezpc@gmail.com>
2020-03-04 23:00:37 +0800

    enter

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+import requests
 from bs4 import BeautifulSoup
-newsoup=BeautifulSoup("<b><!--This is a comment--></b><p>This is not a comment</p>",'html.parser')
-print(newsoup.b.string)
-print(type(newsoup.b.string))
-print(newsoup.p.string)
-print(type(newsoup.p.string))
+url="https://python123.io/ws/demo.html"
+r=requests.get(url)
+demo=r.text
+soup=BeautifulSoup(demo,'html.parser')
+print(soup.head)     # 查看head标签
+# <head><title>This is a python demo page</title></head>
+print(soup.head.contents)     # 查看head标签的孩子节点  
+# [<title>This is a python demo page</title>]
