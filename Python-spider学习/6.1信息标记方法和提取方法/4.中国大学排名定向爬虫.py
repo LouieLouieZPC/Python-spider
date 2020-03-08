696dac72e4378fc81c871f1d763fd03977b4be98
Frank Yeats <louielouiezpc@gmail.com>
2020-03-08 22:19:35 +0800

    enter

--- Python-spider学习/6.1信息标记方法和提取方法/4.中国大学排名定向爬虫.py
+++ Python-spider学习/6.1信息标记方法和提取方法/4.中国大学排名定向爬虫.py
@@ -8,6 +8,7 @@ from fake_useragent import UserAgent
 ua=UserAgent()
 kv={'user-agent':ua.random}
 
+# https://blog.csdn.net/qq_21905401/article/details/78212780 参考
 def getHtmlText(url):
     '''
     从网络上获取大学排名网页内容
@@ -26,20 +27,21 @@ def fillUnivList(ulist,html):
     提取网页内容中的信息到合适的数据结构（列表）
     '''
     soup=BeautifulSoup(html,'html.parser')
-    for tr in soup.find('tbody').children:
+    for tr in soup.find('tbody').children: # 遍历所有tbody下的孩子标签
         if isinstance(tr,bs4.element.Tag): # 查看tr是否为标签
-            tds=tr('td')                   # tr('td')即tr.fina_all('td')
+            tds=tr('td')                   # 获取所有的tr('td')即tr.fina_all('td')
             ulist.append([tds[0].string,tds[1].string,tds[3].string])   # 将数据输入列表中，.string指取标签的字符串
 
 def printUnivList(ulist,num):
     '''
     利用数据结构（列表）战展示并输出结果
     '''
-    print('{:^10}\t{:^6}\t{:^10}'.format('排名','学校名称','总分'))  # \t为横向制表符，^为居中对齐，10为每列的宽度
+    e='{0:^10}\t{1:{3}^10}\t{2:^10}'   # {3}用于填充的单个字符:使用format function 第四个变量填充，即chr(12288),此法可对中英文输出进行优化
+    print(e.format('排名','学校名称','总分',chr(12288)))  # 输出第一行：\t为横向制表符，^为居中对齐，10为每列的宽度
 
     for i in range(num):
         u=ulist[i]
-        print('{:^10}\t{:^6}\t{:^10}'.format(u[0],u[1],u[2]))
+        print(e.format(u[0],u[1],u[2],chr(12288)))
 
 if __name__ == "__main__":
     uinfo=[]
@@ -47,4 +49,3 @@ if __name__ == "__main__":
     html=getHtmlText(url)
     fillUnivList(uinfo,html)
     printUnivList(uinfo,20)  # 20 univs
-
