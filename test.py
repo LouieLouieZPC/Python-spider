ed28f2c89171d63f4db400e8cfb4abf4d55d2cf5
Frank Yeats <louielouiezpc@gmail.com>
2020-03-16 23:36:59 +0800

    enter

--- test.py
+++ test.py
@@ -1,22 +1,3 @@
 import re
-match=re.match(r'[1-9]\d{5}','BIT 100081')
-if match:
-    print(match.group(0))   # 无结果，因为match是个空变量，"returning a Match object, or None if no match was found."
-
-'''
-# Output:
-
-'''
-
-
-print('----------------------------------------')
-
-
-match=re.match(r'[1-9]\d{5}','100081 BIT')
-if match:
-    print(match.group(0))
-
-'''
-# Output:
-100081
-'''
+Is=re.findall(r'[1-9]\d{5}','BIT100081 TSU100084')
+print(Is)
