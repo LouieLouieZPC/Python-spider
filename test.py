223e2f7b1a3166ed8682ce62a25009b2b8e3ad16
Frank Yeats <louielouiezpc@gmail.com>
2020-03-16 23:26:29 +0800

    enter

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-import re # 正则表达式库
-from bs4 import BeautifulSoup
-import  requests
+import re
+match=re.match(r'[1-9]\d{5}','BIT 100081')
+if match:
+    print(match.group(0))   # 无结果，因为match是个空变量，"returning a Match object, or None if no match was found."
 
-r = requests.get("http://python123.io/ws/demo.html")
-demo = r.text
-soup = BeautifulSoup(demo,"html.parser")
+'''
+# Output:
 
-print(soup.find_all(id='link1'))
+'''
+
+
+print('----------------------------------------')
+
+
+match=re.match(r'[1-9]\d{5}','100081 BIT')
+if match:
+    print(match.group(0))
+
+'''
+# Output:
+100081
+'''
