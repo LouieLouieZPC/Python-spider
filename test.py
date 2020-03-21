d5ee8107efc816ebb92e48d6016f2600b510fec0
Frank Yeats <louielouiezpc@gmail.com>
2020-03-21 21:34:26 +0800

    enter

--- test.py
+++ test.py
@@ -1,39 +1,31 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
+#！/user/bin/python
+#-*-coding:utf-8-*-
 
+from fake_useragent import UserAgent
 import requests
 from bs4 import BeautifulSoup
-url="https://python123.io/ws/demo.html"
-r=requests.get(url)
-demo=r.text
+
+ua=UserAgent()
+kv={'user-agent':ua.random}
+
+url='http://www.pythonscraping.com/pages/page3.html'
+
+r=requests.get(url,timeout=30,headers=kv)
+r.raise_for_status()
+r.encoding=r.apparent_encoding
 
 
-import re # 正则表达式库
-from bs4 import BeautifulSoup
-import  requests
-r = requests.get("http://python123.io/ws/demo.html")
-demo = r.text
-soup = BeautifulSoup(demo,"html.parser")
-# 查找demo里的a标签里的href的内容
-for link in soup.find_all('a'):
-    print(link.get('href'))
-# 输出所有a和b标签在列表中
-print(soup.find_all(['a','b']))
-# <tag>.find_all(True)，结果将是当前所有标签信息   
-for tag in soup.find_all(True):
-    print(tag.name)
-# 用正则表达式查找demo里以b开头的所有标签
-for tag in soup.find_all(re.compile('b')):
-    print(tag.name)
-# 查找demo里的p标签里的course的内容
-print(soup.find_all('p','course'))
-# 查找demo里的keyword（关键字参数）id属性中是否包含link1的标签,如果link1没有，将输出空
-print(soup.find_all(id='link1'))
-# 用正则表达式来输出link字符串(以link开头)
-print(soup.find_all(id=re.compile('link')))
-#　recursive是否对子孙全部检索，默认为True，改为False后只能查儿子节点
-print(soup.find_all('a',recursive=False))
-# 检索Basic Python字符串
-print(soup.find_all(string = 'Basic Python'))
-# 用正则表达式检索含有python的所有字符串
-print(soup.find_all(string = re.compile('python')))
+soup=BeautifulSoup(r.text,'html.parser')
+
+
+
+for child in soup.find('table',{'id':'giftList'}).children:  # 得到第一项子标签
+    print(child) 
+
+for siblig in soup.find_all('table',{'id':'giftList'}).tr.next_sibligs:
+    print(siblig)
+
+
+print(soup.find('img',{'scr':'../img/gifts/img1.jpg'}).parent.previous_siblig.get_text())
+
+
