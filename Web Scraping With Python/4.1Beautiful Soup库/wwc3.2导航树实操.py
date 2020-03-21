0be0e63f15f78dd1c5092327f3023b1e6c598d25
Frank Yeats <louielouiezpc@gmail.com>
2020-03-21 22:20:45 +0800

    enter

---
+++ Web Scraping With Python/4.1Beautiful Soup库/wwc3.2导航树实操.py
@@ -0,0 +1,33 @@
+#！/user/bin/python
+#-*-coding:utf-8-*-
+
+from fake_useragent import UserAgent
+import requests
+from bs4 import BeautifulSoup
+
+ua=UserAgent()
+kv={'user-agent':ua.random}
+
+
+def get_htmltext(url):
+    try:
+        r=requests.get(url,timeout=30,headers=kv)
+        r.raise_for_status()
+        r.encoding=r.apparent_encoding
+    except:
+        print('something is error!!!😱')
+    else:
+        bs=BeautifulSoup(r.text,'html.parser')
+
+        for child in bs.find('table',{'id':'giftList'}).children:  # 得到第一项子标签
+            print(child)
+
+        for siblig in bs.find('table',{'id':'giftList'}).tbody.tr.next_sibligs:
+            print(siblig)
+
+        print(bs.find('img',{'scr':'../img/gifts/img1.jpg'}).parent.previous_siblig.get_text())
+
+
+if __name__ == "__main__":
+    url='http://www.pythonscraping.com/pages/page3.html'
+    get_htmltext(url)
--- test.py
+++ test.py
@@ -8,24 +8,26 @@ from bs4 import BeautifulSoup
 ua=UserAgent()
 kv={'user-agent':ua.random}
 
-url='http://www.pythonscraping.com/pages/page3.html'
+
+
 
 r=requests.get(url,timeout=30,headers=kv)
 r.raise_for_status()
 r.encoding=r.apparent_encoding
 
 
-soup=BeautifulSoup(r.text,'html.parser')
+    bs=BeautifulSoup(html,'html.parser')
+
+    for child in bs.find('table',{'id':'giftList'}).children:  # 得到第一项子标签
+        print(child
+    
+    for siblig in bs.find('table',{'id':'giftList'}).tr.next_sibligs:
+        return siblig
+    
+    return (bs.find('img',{'scr':'../img/gifts/img1.jpg'}).parent.previous_siblig.get_text())
 
 
-
-for child in soup.find('table',{'id':'giftList'}).children:  # 得到第一项子标签
-    print(child) 
-
-for siblig in soup.find_all('table',{'id':'giftList'}).tr.next_sibligs:
-    print(siblig)
-
-
-print(soup.find('img',{'scr':'../img/gifts/img1.jpg'}).parent.previous_siblig.get_text())
-
-
+if __name__ == "__main__":
+    url='http://www.pythonscraping.com/pages/page3.html'
+    html=get_htmltext(url)
+    print(parser_page(html))
