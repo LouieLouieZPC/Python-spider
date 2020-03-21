1ab584c75b4fe95bb072b9a83bcc7310346427db
Frank Yeats <louielouiezpc@gmail.com>
2020-03-21 13:37:59 +0800

    enter

--- Web Scraping With Python/5.1信息标记方法和提取方法/3.2用urlopen和bs4抓取一个网站.py
+++ Web Scraping With Python/5.1信息标记方法和提取方法/3.2用urlopen和bs4抓取一个网站.py
@@ -16,5 +16,5 @@ namelist=soup.find_all('span',{'class':'green'})   # 找到所有符合条件的
 for name in namelist:          # 遍历namelist
     print(name.get_text())     # .get_text()会将所有的超链接、段落、标签清除，只剩下一串不带标签的文字
 
-namelist1=soup.find_all(text='the prince')
-print(len(namelist1))
+namelist1=soup.find_all(text='the prince')  # 用标签的text文本内容去匹配
+print(len(namelist1))  # 打印出匹配到'the prince'内容的标签数量
