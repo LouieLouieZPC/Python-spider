d84f1bf847951241778ff421ac49526732ad21da
Frank Yeats <louielouiezpc@gmail.com>
2020-03-22 00:29:40 +0800

    eenter

--- Web Scraping With Python/7.1定向爬虫制作示例/淘宝商品信息定向爬虫.py
+++ Web Scraping With Python/7.1定向爬虫制作示例/淘宝商品信息定向爬虫.py
@@ -3,10 +3,26 @@
 
 import re
 import requests
+from fake_useragent import UserAgent
 
-def getHtmlText(url):
-    print('')
 
+
+
+def getHtmlText(url):\
+    # 部署代理
+    ua=UserAgent()
+    kv={'user-agent':'ua.random'}
+    
+    # 部署cookies
+    request_cookies=requests.get
+
+    try:
+        r=requests.get(url,timeout=30,headers=kv)
+        r.raise_for_status()
+        r.encoding=r.apparent_encoding
+        return r.text
+    except:
+        return ''
 
 def paserPage(ilt,html):
     print('')
@@ -26,6 +42,6 @@ if __name__ == "__main__":
             url=start_url+ '&s=' + str(44*i)
             html=getHtmlText(url)
             paserPage(inforList,html)
-        except:
+        except:  # 若某一页面解析有问题，则跳过
             continue
     printGoodsList(inforList)
