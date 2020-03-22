5cbee72d949ccdd619f65d19f2ad90e148f82dad
Frank Yeats <louielouiezpc@gmail.com>
2020-03-22 23:36:56 +0800

    enter

---
+++ Web Scraping With Python/7.1定向爬虫制作示例/3.1股票数据定向爬虫.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import traceback
+import re
+
+# 获得url对应的页面
+def getHTMLText(url):
+    return('')
+
+# 获得股票信息列表
+def getStockList(lst,stockURl):
+    '''
+    第一个参数为列表类型，第二个参数为获得股票url的网站
+    '''
+    return('')
+
+
+def getStocInfo(lst,stockURl,fpath):
+    '''
+    三个参数：保存所有股票的信息列表，获得股票信息的url网站，把文件存到文件的文件路径
+    '''
+    return('')
+
+
+def main():
+    
