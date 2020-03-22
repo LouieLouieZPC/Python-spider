2c72982fc8976a86e9e15d91c49c8156ea09646b
Frank Yeats <louielouiezpc@gmail.com>
2020-03-22 20:36:39 +0800

    enter

--- Web Scraping With Python/7.1定向爬虫制作示例/1.1★淘宝商品信息定向爬虫.py
+++ Web Scraping With Python/7.1定向爬虫制作示例/1.1★淘宝商品信息定向爬虫.py
@@ -6,18 +6,16 @@ import requests
 from fake_useragent import UserAgent
 
 
-
-
-def getHtmlText(url):\
+def getHtmlText(url):
     # 部署代理
     ua=UserAgent()
     kv={'user-agent':'ua.random'}
     
     # 部署cookies
-    request_cookies=requests.get
+    ck={'cookie':'t=8d41af525be6d60e1d40814674d8820f; cna=9dnxFsgkIAcCAbeAITjWCrnN; _m_h5_tk=2b59e81209230ce0623bd4421057f1fd_1584595870333; _m_h5_tk_enc=4ef52ed9a0b7cd7127e4e0480946ff9c; thw=cn; sgcookie=ETmpEUOWeot1QvhNHMYw5; uc3=lg2=U%2BGCWk%2F75gdr5Q%3D%3D&id2=UNX%2FRabQf7RKzA%3D%3D&nk2=D8zlh3k8M9o5Pg%3D%3D&vt3=F8dBxd9koY4EyuRS5Zg%3D; lgc=louielouie; uc4=nk4=0%40De%2FPBpYsSfK9SgJzWf2E343jTyWA&id4=0%40UgJ6xtNFD8seIyRycNZIK1sxxV9L; tracknick=louielouie; _cc_=V32FPkk%2Fhw%3D%3D; tg=0; enc=FgUj65srs%2BHu2039WlqZz99vL7Mm138iexBU7ZHcxEyTuCm1c%2BUMQpp5fkVBImpL6H00Sn4In%2FiRo54Uah74sA%3D%3D; tfstk=c1bfB3AAMxDfisAj0GNy7LkTpDTOZ0wWZo9AGgOGJXkoid5fiO0edQlcjY0JvQ1..; mt=ci=-1_1; hng=CN%7Czh-CN%7CCNY%7C156; v=0; cookie2=5b37e99960f293f573ace663c80e5672; uc1=cookie14=UoTUPvgy4sbPow%3D%3D; _tb_token_=f7675165ebbde; JSESSIONID=FED28730AC1CD53DC72205AA96370D93; alitrackid=www.taobao.com; lastalitrackid=www.taobao.com; isg=BDQ0Z5331hAMO0LY7cnE_hbdBfKmDVj3zLPiC86Ve79TOdeD9h9Rh_37vXHhwZBP; l=dBPZQQJqQ1ZVZq1DBOfNNDE4CkbTwIRf1sPr9ZAIDICPOX16PX7RWZ4olTYBCnGVHsGBR3oGfmNDBb86lydqGhZfP3k_J_qq3dC..; _samesite_flag_=true'}
 
     try:
-        r=requests.get(url,timeout=30,headers=kv)   # 获取
+        r=requests.get(url,timeout=30,headers=kv,headers=ck)       # 获取
         r.raise_for_status()
         r.encoding=r.apparent_encoding
         return r.text                               # 获取内容
@@ -46,6 +44,8 @@ def printGoodsList(ilt):
     for i in ilt:
         count+=1
         print(tplt.format(count,ilt[0],ilt[1]))
+
+
 
 if __name__ == "__main__":
     goods='书包'
