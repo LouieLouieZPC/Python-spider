aa1311fe1efd1891768a712f69f51329eaf8eece
Frank Yeats <louielouiezpc@gmail.com>
2020-03-24 23:06:41 +0800

    enter

--- Web Scraping With Python/7.1定向爬虫制作示例/3.1股票数据定向爬虫.py
+++ Web Scraping With Python/7.1定向爬虫制作示例/3.1股票数据定向爬虫.py
@@ -2,25 +2,60 @@ import requests
 from bs4 import BeautifulSoup
 import traceback
 import re
+from fake_useragent import UserAgent
 
-# 获得url对应的页面
-def getHTMLText(url):
-    return('')
 
+# 获得url对应的页面内容
+def getHTMLText(url,code='utf-8'):
+    # 配置代理
+    ua=UserAgent()
+    kv={'user-agent':ua.random}
+    
+    try:
+        r=requests.get(url,timeout=30,headers=kv)
+        r.raise_for_status()
+        r.encoding=code      # 直接赋，提高速度
+        return r.text
+    except:
+        return''
+        
 # 获得股票信息列表
 def getStockList(lst,stockURl):
     '''
-    第一个参数为列表类型，第二个参数为获得股票url的网站
+    第一个参数为存储在的列表类型，第二个参数为获得股票url的网站
     '''
-    return('')
+    html=getHTMLText(stockURl，'GB2312')
+    soup=BeautifulSoup(html,'html.parser')
+    a=soup.findAll('a')
+    for i in a:
+        try:
+            href=i.attrs['href']
+            lst.append(re.findall(r'[s][hz]\d{6}',href)[0])   # [0]是为了取出返回的值添加，不然会将空值以空列表的形式添加进去
+        except:
+            continue
 
 
-def getStocInfo(lst,stockURl,fpath):
+def getStockInfo(lst,stockURl,fpath):
     '''
     三个参数：保存所有股票的信息列表，获得股票信息的url网站，把文件存到文件的文件路径
     '''
-    return('')
+    for stock in lst:    # 遍历股票列表
+        url=stockURl+stock
+        html=getHTMLText(url)
+        try:
+            if html=='':   # 可能没有这个页面
+                continue
+            infoDict={}    # 采用字典的数据结构放结果
+            soup=BeautifulSoup(html,'html.parser')
+            stockInfo=soup.find('')
 
 
-def main():
-    
+
+
+if __name__ == "__main__":
+    stock_list_url='http://quote.eastmoney.com/stock_list.html'  # 获取股票代码列表的网站
+    stock_info_url='https://xueqiu.com/S/' # 获取股票信息的网站
+    output_file='D：//股票信息.text'
+    slist=[]
+    getStockList(slist,stock_list_url)
+    getStockInfo(slist,stock_info_url,output_file)
