d84feb5e7e4426f4416e5e777bf2f6fbb10030ab
Frank Yeats <louielouiezpc@gmail.com>
2020-04-28 21:24:12 +0800

    enter

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 import pandas as pd
-d={'one':pd.serise()}
+
+df = pd.DataFrame([[1, 2], [3, 4]], columns = ['a','b'])
+df2 = pd.DataFrame([[5, 6], [7, 8]], columns = ['a','b'])
+
+df = df.append(df2)
+
+# Drop rows with label 0
+df = df.drop(0)
+
+print (df)
