17f61ff82fe12b95f6bb1d7260324fb8ae78b883
Frank Yeats <louielouiezpc@gmail.com>
2020-04-28 19:43:09 +0800

    enter

--- test.py
+++ test.py
@@ -1,4 +1,2 @@
 import pandas as pd
-data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
-df = pd.DataFrame(data, index=['rank1','rank2','rank3','rank4']) # 注：index参数为每行分配一个索引。索引的长度应等于数组的长度
-print (df)
+d={'one':pd.serise()}
