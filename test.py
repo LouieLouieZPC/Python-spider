4e9f48b0ed549e18baca53417bc92eba60b79bb5
Frank Yeats <louielouiezpc@gmail.com>
2020-04-28 19:28:48 +0800

    enter

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import pandas as pd
-data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]} # 往往字典的键都作为列标题
-df = pd.DataFrame(data)
+data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
+df = pd.DataFrame(data, index=['rank1','rank2','rank3','rank4']) # 注：index参数为每行分配一个索引。索引的长度应等于数组的长度
 print (df)
