608980e692206f14a1b19a124349976632626d11
Frank Yeats <louielouiezpc@gmail.com>
2020-04-29 19:15:14 +0800

    enter

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
+#!user/bin/python
+#-*-coding:utf-8-*-
+
 import pandas as pd
 
-df = pd.DataFrame([[1, 2], [3, 4]], columns = ['a','b'])
-df2 = pd.DataFrame([[5, 6], [7, 8]], columns = ['a','b'])
-
-df = df.append(df2)
-
-# Drop rows with label 0
-df = df.drop(0)
-
-print (df)
+df=pd.DataFrame([[1,2],[3,4]],columns=['a','b'])
+print(df)
+print('----------------------------------------')
+df1=pd.DataFrame([[5,6],[7,8]],columns=['a','b'])
+df=df.append(df1)
+print(df)
