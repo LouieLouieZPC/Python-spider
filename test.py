add07c6249644fc5841ba67db300be87c7c98b42
Frank Yeats <louielouiezpc@gmail.com>
2020-05-06 13:22:19 +0800

    enter

--- test.py
+++ test.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
 #-*-coding:utf-8-*-
 
-# creating an empty panel
-import pandas as pd
-import numpy as np
-data = {'Item1' : pd.DataFrame(np.random.randn(4, 3)), 
-        'Item2' : pd.DataFrame(np.random.randn(4, 2))}
-p = pd.Panel(data)
-print (p.minor_xs(1))
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+ua=UserAgent()  # 代理
+kv={'user-agent':ua.random} # 随机
+
+def getHtml(url):
+        '获取HTML'
+        r=requests.get(url,timeout=30,headers=kv)
+        r.raise_for_status()
+        r.encoding='utf-8'
+        return r.text
+
+def paserHtml(html):
+        '''解析HTML''' 
+        bs=BeautifulSoup(html,'html.parser')   
+        ilt=bs.find('ul',attrs={'class':'list_con_li autoHeight'})
+        ilt2=ilt.find_all('a')
+        chapter_names=[]
+        chapter_urls=[]
+        for comic in ilt2:
+                name=comic.text
+                href=comic.get('href')
+                chapter_names.insert(0,name)
+                chapter_urls.insert(0,href)
+        print(chapter_names)
+        print(chapter_urls)
+
+if __name__ == "__main__":
+    url='https://www.dmzj.com/info/yaoshenji.html'
+    html=getHtml(url)
+    content=paserHtml(html)
+    print(content)
